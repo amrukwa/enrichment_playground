c81bc3d60e27643c679cda3e67af99b2868d2687
amrukwa <a.mrukwa00@gmail.com>
2021-02-23 22:54:01 +0100

    Speed up the ranks

--- source/ranks.R
+++ source/ranks.R
@@ -2,11 +2,13 @@ library(matrixStats)
 
 rank_genes <- function(data, labels, rank='s2n'){
   d_mat <- as.matrix(data)
-  control_means <- rowMeans(d_mat[, labels$Group=="c"])
-  disease_means <- rowMeans(d_mat[, labels$Group=="d"])
+  control <- d_mat[, labels$Group=="c"]
+  disease <- d_mat[, labels$Group=="d"]
+  control_means <- rowMeans(control)
+  disease_means <- rowMeans(disease)
   if (rank == 's2n'){
-    control_sds <- rowSds(d_mat[, labels$Group=="c"])
-    disease_sds <- rowSds(d_mat[, labels$Group=="d"])
+    control_sds <- rowSds(control)
+    disease_sds <- rowSds(disease)
     ranks <- (disease_means - control_means)/(control_sds+disease_sds)
   }else if(rank=='lfc'){
     ranks <- log(disease_means/control_means)
