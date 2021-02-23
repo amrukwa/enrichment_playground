5da6475897f9cce33f28fdc9a0c02a04a9bec377
amrukwa <a.mrukwa00@gmail.com>
2021-02-23 13:58:09 +0100

    Speed up P_hit

--- source/gsea.R
+++ source/gsea.R
@@ -12,12 +12,13 @@ ES_i <- function(hit, norm_rank, miss_increment, prev_es){
 }
 
 
-get_ES <- function(data, geneset, labels, miss_increment, is_hit, rank = "s2n", absolute=TRUE){
+get_ES <- function(data, geneset, labels, miss_increment, rank = "s2n", absolute=TRUE){
   data$ranks <- rank_genes(data, labels, rank)
   if (absolute){
     data$ranks <- abs(data$ranks)
   }
   data = data[order(-data$ranks), ]
+  is_hit <- row.names(data)  %in% geneset$GENES$ID
   N_R <- sum(data[is_hit, "ranks"])
   es <- ES_i(is_hit[1], (data[1, "ranks"]/N_R), miss_increment, 0)
   ES <- es
@@ -32,20 +33,14 @@ get_ES <- function(data, geneset, labels, miss_increment, is_hit, rank = "s2n", 
 
 
 gsea <- function(data, geneset, labels, rank = "s2n", absolute=TRUE, n_perm=1000){
-  cores=detectCores()
-  cl <- makeCluster(cores[1]-1)
-  clusterExport(cl, c("get_ES", "rank_genes", "ES_i"))
-  registerDoParallel(cl)
-  
   N <- nrow(data)
   N_H <- length(geneset$GENES$ID)
   miss_increment <- 1/(N-N_H)
-  is_hit <- row.names(data)  %in% geneset$GENES$ID
   
-  ES <- get_ES(data, geneset, labels, miss_increment, is_hit, rank, absolute)
+  ES <- get_ES(data, geneset, labels, miss_increment, rank, absolute)
   better <- foreach (i=1:n_perm, .combine='+', .export= c("get_ES", "rank_genes", "ES_i"), .packages=c("matrixStats")) %dopar% {
     shuffled_labels <- transform( labels, Group = sample(Group) )
-    es <- get_ES(data, geneset, shuffled_labels, miss_increment, is_hit, rank, absolute)
+    es <- get_ES(data, geneset, shuffled_labels, miss_increment, rank, absolute)
     better_one <- 0
     if (es > ES){
       # the stuff taking the sign into account, now only for absolute bigger
@@ -53,6 +48,5 @@ gsea <- function(data, geneset, labels, rank = "s2n", absolute=TRUE, n_perm=1000
     }
     better_one
   }
-  stopCluster(cl)
   p_val <- better/n_perm
 }
