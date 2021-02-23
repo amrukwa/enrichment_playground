2bf7e7d698c4bd48936b842e74d74cde48c56141
amrukwa <a.mrukwa00@gmail.com>
2021-02-23 11:19:08 +0100

    Speed up row operations

--- source/ranks.R
+++ source/ranks.R
@@ -1,10 +1,10 @@
 library(matrixStats)
 
 rank_genes <- function(data, labels, rank='s2n'){
-  control_means <- rowMeans(data[, labels$Group=="c"])
-  disease_means <- rowMeans(data[, labels$Group=="d"])
+  d_mat <- as.matrix(data)
+  control_means <- rowMeans(d_mat[, labels$Group=="c"])
+  disease_means <- rowMeans(d_mat[, labels$Group=="d"])
   if (rank == 's2n'){
-    d_mat <- as.matrix(data)
     control_sds <- rowSds(d_mat[, labels$Group=="c"])
     disease_sds <- rowSds(d_mat[, labels$Group=="d"])
     ranks <- (disease_means - control_means)/(control_sds+disease_sds)
