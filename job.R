367c92285f763d173d3071e580ac602a4974eb58
amrukwa <a.mrukwa00@gmail.com>
2021-03-05 09:00:38 +0000

    Get the results

--- job.R
+++ job.R
@@ -1,7 +1,22 @@
+library(foreach)
+library(doParallel)
+source("source/gsea_polyaxon.R")
+
 load(".RData")
 colnames(genesets) = c("ID", "Title", "features")
 source("source/gsea_polyaxon.R")
-s2n_abs <- gsea(data, genesets, metaInfo, rank = 's2n')
+
+cores=detectCores()
+cl <- makeCluster(cores[1])
+clusterExport(cl, c("get_ES", "rank_genes", "single_gsea", "get_miss_increments", "get_miss_inc"))
+registerDoParallel(cl)
+
+# make it parallel here - progress bar should work then
+# s2n_abs <- gsea(data, genesets, metaInfo, rank = 's2n')
 # s2n <- gsea(data, genesets, metaInfo, rank = 's2n', absolute=FALSE)
 # lfc_abs <- gsea(data, genesets, metaInfo, rank = 'lfc')
-# lfc <- gsea(data, genesets, metaInfo, rank = 'lfc', absolute=FALSE)
+lfc <- gsea(data, genesets, metaInfo, rank = 'lfc', absolute=FALSE)
+
+stopCluster(cl)
+
+save(lfc_abs, lfc, s2n_abs, s2n, file = "results.RData")
--- source/gsea_polyaxon.R
+++ source/gsea_polyaxon.R
@@ -1,26 +1,29 @@
 source("source/ranks.R")
-library(foreach)
-library(doParallel)
 
-get_ES <- function(geneset, data, labels){
-  data = data[order(-data$ranks), ]
-  is_hit <- row.names(data)  %in% unlist(strsplit(geneset["features"], ","))
-  N_R <- sum(abs(data[is_hit, "ranks"]))
-  P_hit <- rep(0, each=nrow(data))
-  P_hit[is_hit] <- (abs(data[is_hit, "ranks"])/N_R)
+get_ES <- function(geneset, ranks, genes, labels){
+  is_hit <- genes  %in% unlist(strsplit(geneset["features"], ","))
+  N_R <- sum(ranks[is_hit]) 
+  ranks[is_hit] = ranks[is_hit]/N_R
+  es <- 0
+  ES <- es
   m_inc <- as.numeric(geneset["miss_increment"])
-  P_miss <- rep(m_inc, each=nrow(data))
-  P_miss[is_hit] <- 0
-  ES <- cumsum(P_hit - P_miss)
-  ES[which.max(abs(ES))]
+  for (i in 1:length(ranks)){
+    es <- if(is_hit[i]) (es+ranks[i]) else (es-m_inc)
+    if(abs(es) > abs(ES)){
+      ES <- es
+    }
+  }
+  ES
 }
 
-get_miss_inc <- function(geneset, N){
-  increment <- 1/(N- length(unlist(strsplit(geneset["features"], ","))))
+get_miss_inc <- function(geneset, genes){
+  N = length(genes)
+  in_gs <- which(genes %in% unlist(strsplit(geneset["features"], ",")))
+  increment <- 1/(N- length(in_gs))
   }
 
-get_miss_increments <- function(genesets, N){
-  increments <- apply(genesets, 1, get_miss_inc, N=N)
+get_miss_increments <- function(genesets, genes){
+  increments <- apply(genesets, 1, get_miss_inc, genes=genes)
 }
 
 single_gsea <- function(data, genesets, labels, rank = "s2n", absolute=TRUE){
@@ -28,38 +31,35 @@ single_gsea <- function(data, genesets, labels, rank = "s2n", absolute=TRUE){
   if (absolute){
     data$ranks <- abs(data$ranks)
   }
-  ES <- apply(genesets, 1, get_ES, data=data, labels=labels)
+  data = data[order(-data$ranks), ]
+  genes = row.names(data)
+  ranks = abs(data$ranks)
+  ES <- apply(genesets, 1, get_ES, ranks=ranks, genes=genes, labels=labels)
   ES
 }
 
-gsea <- function(data, genesets, labels, rank = "s2n", absolute=TRUE, n_perm=1000){
-  genesets$miss_increment <- get_miss_increments(genesets, nrow(data))
-  results <- data.frame(ID=genesets$ID, Title=genesets$Title) 
-  results$ES <- single_gsea(data, genesets, labels, rank, absolute)
-  
-  cores=detectCores()
-  cl <- makeCluster(cores[1])
-  clusterExport(cl, c("get_ES", "rank_genes", "single_gsea", "get_miss_increments", "get_miss_inc"))
-  registerDoParallel(cl)
+gsea <- function(data, gs, labels, rank = "s2n", absolute=TRUE, n_perm=1000){
+  genes <- row.names(data)
+  gs$miss_increment <- get_miss_increments(gs, genes)
+  results <- data.frame(ID=gs$ID, Title=gs$Title) 
+  ES <- single_gsea(data, gs, labels, rank, absolute)
   
   pvals <- foreach(i=1:n_perm, .combine='+') %do% { 
     shuffled_labels <- transform( labels, Group = sample(Group) )
-    es <- single_gsea(data, genesets, shuffled_labels, rank, absolute)
-    better <- rep(0, nrow(genesets))
-    for (i in 1:nrow(genesets)){
-      if(results[i, "ES"] > 0 & results[i, "ES"] < es[i]){
+    es <- single_gsea(data, gs, shuffled_labels, rank, absolute)
+    better <- rep(0, nrow(gs))
+    for (i in 1:nrow(gs)){
+      if(ES[i] > 0 & ES[i] < es[i]){
         better[i] <- 1
-      }else if(results[i, "ES"] < 0 & results[i, "ES"] > es[i]){
+      }else if(ES[i] < 0 & ES[i] > es[i]){
         better[i] <- 1
       }
     }
     better
   }
-
-  stopCluster(cl)
-  
+  results$ES <- ES
   results$pval <- pvals/n_perm
-  results$corrected_pval <- p.adjust(results$pval, method="BH")
+  #results$corrected_pval <- p.adjust(results$pval, method="BH")
   results
 }
 
