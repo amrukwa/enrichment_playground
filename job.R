c8273c56bcf3f15d1705cf971b3d7d86de12a5fb
amrukwa <a.mrukwa00@gamil.com>
2021-03-07 20:59:18 +0100

    Fix the GSEA file for better usage

--- job.R
+++ job.R
@@ -4,14 +4,12 @@ source("source/gsea_polyaxon.R")
 
 load(".RData")
 colnames(genesets) = c("ID", "Title", "features")
-source("source/gsea_polyaxon.R")
 
 cores=detectCores()
 cl <- makeCluster(cores[1])
 clusterExport(cl, c("get_ES", "rank_genes", "single_gsea", "get_miss_increments", "get_miss_inc"))
 registerDoParallel(cl)
 
-# make it parallel here - progress bar should work then
 # s2n_abs <- gsea(data, genesets, metaInfo, rank = 's2n')
 # s2n <- gsea(data, genesets, metaInfo, rank = 's2n', absolute=FALSE)
 # lfc_abs <- gsea(data, genesets, metaInfo, rank = 'lfc')
@@ -19,4 +17,4 @@ lfc <- gsea(data, genesets, metaInfo, rank = 'lfc', absolute=FALSE)
 
 stopCluster(cl)
 
-save(lfc_abs, lfc, s2n_abs, s2n, file = "results.RData")
+#save(lfc_abs, lfc, s2n_abs, s2n, file = "results.RData")
