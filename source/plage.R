b97a97846bdb20645edb1a47f506b06af29ad0f8
amrukwa <a.mrukwa00@gamil.com>
2021-03-18 23:41:58 +0100

    Add PLAGE to comparison

--- source/plage.R
+++ source/plage.R
@@ -4,7 +4,6 @@ decomposition <- function(dataset, geneset){
   genes <- rownames(dataset) %in% geneset$GENES$ID
   x <- t(dataset[genes,])
   values <- prcomp(x, center=TRUE, scale.=TRUE, rank.=1)$x
-  
   t(values)
 }
 
