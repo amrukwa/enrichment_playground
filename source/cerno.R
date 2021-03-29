36c94bd7af62952d9ff0b6ef50f885a777339a84
amrukwa <a.mrukwa00@gamil.com>
2021-03-29 11:56:41 +0200

    Separate visualizations and basics for CERNO heatmaps

--- source/cerno.R
+++ source/cerno.R
@@ -1,4 +1,17 @@
+library(plotly)
+
 # as in tmod the function takes already sorted gene list, my implementation will do the same
+
+rank_patient_genes <- function(gene_expressions, sort_type="abs", gene_names){
+  if(sort_type=="abs"){
+    ordered <- gene_names[order(abs(gene_expressions), decreasing = TRUE)]
+  }else if(sort_type=="htl"){
+    ordered <- gene_names[order(gene_expressions, decreasing = TRUE)]
+  }else{
+    ordered <- gene_names[order(gene_expressions)]
+  }
+  ordered
+}
 
 single_cerno <- function(ordered_labels, geneset, combining_method="fisher"){
   N <- length(ordered_labels) # all analyzed genes
@@ -23,6 +36,39 @@ single_cerno <- function(ordered_labels, geneset, combining_method="fisher"){
     pval <- pnorm(stat_val)
     return (data.frame(z=stat_val, N_gs = N_gs, AUC=AUC, pval=pval))
   }
+}
+
+cerno_heatmaps <- function(dataset, genesets){
+  gene_names <- rownames(dataset)
+  patient_n <- length(dataset)
+  plots <- list("CERNO_ABS", "CERNO_HTL", "CERNO_LTH")
+  
+  # get ranks for every patient
+  x_normalized <- t(apply(dataset, 1, function(x) pnorm(x, mean=mean(x), sd=sd(x), lower.tail = TRUE)))
+  abs_labels <- apply(x_normalized, 2, rank_patient_genes, sort_type="abs", gene_names=gene_names)
+  htl_labels <- apply(x_normalized, 2, rank_patient_genes, sort_type="htl", gene_names=gene_names)
+  lth_labels <- apply(x_normalized, 2, rank_patient_genes, sort_type="lth", gene_names=gene_names)
+  # for each pathway calculate values
+  for (i in 1:length(pathways)){
+    pathway <- pathways[i]
+    title <- pathway$MODULES$Title
+    genes <- rownames(dataset) %in% pathway$GENES$ID
+    CERNO_abs <- c()
+    CERNO_htl <- c()
+    CERNO_lth <- c()
+    # parallelize it
+    for (j in 1:patient_n){
+      abs_res <- single_cerno(abs_labels[,j], pathway)
+      htl_res <- single_cerno(htl_labels[,j], pathway)
+      lth_res <- single_cerno(lth_labels[,j], pathway)
+      CERNO_abs <- c(CERNO_abs, abs_res[, "F"])
+      CERNO_htl <- c(CERNO_htl, htl_res[, "F"])
+      CERNO_lth <- c(CERNO_lth, lth_res[, "F"])
+      # save pvals as well
+    }
+  }
+  # save the results as 6 matrixes <- pvals and Fs x6
+  # x, y for heatmaps <- pathway names, patient numbers
 }
 
 cerno <- function(ordered_labels, genesets, combining_method="fisher"){
--- source/visualization.R
+++ source/visualization.R
@@ -15,17 +15,6 @@ annotation_style <- list(
   font = list(size = 15)
 )
 
-rank_patient_genes <- function(gene_expressions, sort_type="abs", gene_names){
-  if(sort_type=="abs"){
-    ordered <- gene_names[order(abs(gene_expressions), decreasing = TRUE)]
-  }else if(sort_type=="htl"){
-    ordered <- gene_names[order(gene_expressions, decreasing = TRUE)]
-  }else{
-    ordered <- gene_names[order(gene_expressions)]
-  }
-  ordered
-}
-
 visualize_pathways <- function(dataset, labels, pathways, GSVA_ES){
   # prepare for visualizations
   x_umap <- umap(t(dataset))
@@ -37,18 +26,11 @@ visualize_pathways <- function(dataset, labels, pathways, GSVA_ES){
   df$Group <- groups
   patient_n <- length(dataset)
   plots1 <- list("Group", "GSVA", "Z", "SVD")
-  plots2 <- list("Group", "CERNO_ABS", "CERNO_HTL", "CERNO_LTH")
-  gradients <- list(plots1, plots2)
   
-  # normalization of each row (Z scores for Z and CERNO)
+  # normalization of each row (Z scores)
   x_normalized <- t(apply(dataset, 1, function(x) pnorm(x, mean=mean(x), sd=sd(x), lower.tail = TRUE)))
-  # prepare ranks for CERNO
-  gene_names <- rownames(dataset)
-  abs_labels <- apply(x_normalized, 2, rank_patient_genes, sort_type="abs", gene_names=gene_names)
-  htl_labels <- apply(x_normalized, 2, rank_patient_genes, sort_type="htl", gene_names=gene_names)
-  lth_labels <- apply(x_normalized, 2, rank_patient_genes, sort_type="lth", gene_names=gene_names)
   # prepare for subplots
-  subplots <- vector(mode = "list", length = length(gradients))
+  subplots <- vector(mode = "list", length = length(pathways))
   
   for (i in 1:length(pathways)){
     pathway <- pathways[i]
@@ -63,42 +45,22 @@ visualize_pathways <- function(dataset, labels, pathways, GSVA_ES){
     x_norm <- x_normalized[genes,]
     # Z
     df$Z <- colSums(x_norm)/sqrt(N)
-    # CERNO
-    CERNO_abs <- c()
-    CERNO_htl <- c()
-    CERNO_lth <- c()
-    for (j in 1:patient_n){
-      abs_res <- single_cerno(abs_labels[,j], pathway)
-      htl_res <- single_cerno(htl_labels[,j], pathway)
-      lth_res <- single_cerno(lth_labels[,j], pathway)
-      CERNO_abs <- c(CERNO_abs, abs_res[, "F"])
-      CERNO_htl <- c(CERNO_htl, htl_res[, "F"])
-      CERNO_lth <- c(CERNO_lth, lth_res[, "F"])
-    }
-    df$CERNO_ABS <- CERNO_abs
-    df$CERNO_HTL <- CERNO_htl
-    df$CERNO_LTH <- CERNO_lth
-    fig <- group_comparison(df, gradients)
+
+    fig <- group_comparison(df, plots1)
     subplots[[i]] <- fig
   }
   subplots
 }
 
 group_comparison <- function(df, gradients){
-  pos <- 1
-  plots <- vector(mode = "list", length = length(gradients))
-  for (group in gradients){
-    figs <- vector(mode = "list", length = length(group))
-    for (i in 1:length(group)){
-      gradient_name <- group[[i]]
-      figs[[i]] <- plot_ly(df, x = ~UMAP1, y = ~UMAP2, type = 'scatter', color= as.formula(paste0('~', gradient_name)), 
-                           name=gradient_name, text = as.formula(paste0('~', gradient_name)),
-                           mode = 'markers', showlegend=FALSE) %>% layout(annotations = c(annotation_style, text=gradient_name))
-    }
-    fig <- subplot(figs, nrows = 2, shareX = TRUE, shareY = TRUE)
-    fig <- fig %>% layout(autosize = F, width = 725, height = 500)
-    plots[[pos]] <- fig
-    pos = pos+1
+  figs <- vector(mode = "list", length = length(gradients))
+  for (i in 1:length(gradients)){
+    gradient_name <- gradients[[i]]
+    figs[[i]] <- plot_ly(df, x = ~UMAP1, y = ~UMAP2, type = 'scatter', color= as.formula(paste0('~', gradient_name)),
+                         name=gradient_name, text = as.formula(paste0('~', gradient_name)),
+                         mode = 'markers', showlegend=FALSE) %>% layout(annotations = c(annotation_style, text=gradient_name))
   }
-  plots
+  fig <- subplot(figs, nrows = 2, shareX = TRUE, shareY = TRUE)
+  fig <- fig %>% layout(autosize = F, width = 725, height = 500)
+  fig
 }
